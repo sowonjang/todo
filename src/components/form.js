e7577510f87a2537d619190e471ab6c38a76ee06
sowonjang <sowon4779@gmail.com>
2021-08-26 20:26:53 +0900

    add: file

---
+++ src/components/form.js
@@ -0,0 +1,9 @@
+export const form = () => {
+  return `
+    <form>
+      <h2 class="add_item">할일 추가</h2>
+      <input type="text" class="title" placeholder="내용을 입력하세요">
+      <button>등록하기</button>
+    </form>
+  `
+}
---
+++ src/components/list.js
@@ -0,0 +1,15 @@
+export const list = () => {
+  return `
+    <div class="list">
+      <div class="title">
+        <input type="checkbox" value="true">
+        <p>내용</p>
+      </div>
+      <p>2021-08-22</p>
+      <div class="btn_box">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  `
+}
